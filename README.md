af065befa83b8ff1adde0b79c262ccadbf5c315f
John Griffiths <john@uxgent.co>
2015-04-20 14:10:37 -0400

    Init.

---
+++ README.md
@@ -0,0 +1,2 @@
+# meteor-svginjector
+Fast, caching, dynamic inline SVG DOM injection library ~ packaged for Meteor
---
+++ package.js
@@ -0,0 +1,13 @@
+Package.describe({
+  name: 'johnantoni:meteor-svginjector',
+  version: '0.0.1',
+  summary: 'Fast, caching, dynamic inline SVG DOM injection library ~ packaged for Meteor',
+  git: 'https://github.com/johnantoni/meteor-svginjector.git',
+  documentation: 'README.md'
+});
+
+Package.on_use(function(api) {
+  api.add_files([
+    'SVGInjector/svginjector.js'
+  ], ['client']);
+});
